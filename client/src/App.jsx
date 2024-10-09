aeefbeeee2504828969429366a1f8bffe85f59b2
MegeKaplan <kaplanmehmetege@gmail.com>
2024-10-09 18:59:05 +0300

    project configuration completed

---
+++ client/src/App.jsx
@@ -0,0 +1,9 @@
+function App() {
+  return (
+    <div className="w-full h-[100vh] flex items-center justify-center bg-amber-300">
+      <h1 className="text-9xl text-gray-900 font-black">Hello Flare!</h1>
+    </div>
+  );
+}
+
+export default App
