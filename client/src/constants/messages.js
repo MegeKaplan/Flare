8244f4f3732328a489cab4cdbd038c3d29e7999b
MegeKaplan <kaplanmehmetege@gmail.com>
2024-10-29 00:57:39 +0300

    new post page design completed

--- client/src/constants/messages.js
+++ client/src/constants/messages.js
@@ -7,4 +7,7 @@ export const MESSAGES = {
   PASSWORD_REQUIRED: "Şifre boş bırakılamaz!",
   PASSWORD_LENGTH: (minLength) =>
     `Şifre en az ${minLength} karakter olmalıdır!`,
+  FILE_LIMIT: (limit) => `En fazla ${limit} resim seçebilirsiniz!`,
+  IMAGE_REQUIRED: "En az bir resim seçmelisiniz!",
+  POST_CONTENT_REQUIRED: "Gönderi içeriği boş bırakılamaz!",
 };
